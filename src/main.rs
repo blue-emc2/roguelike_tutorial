b39d6093212614061b8d6a9862e0510ea382985f
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2020-08-12 22:37:02 +0900

    部屋を2つ作ってトンネルをつなぐ

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod map;
 pub use map::*;
 mod player;
 pub use player::*;
+mod rect;
+pub use rect::*;
 
 pub struct State {
   ecs: World,
@@ -47,7 +49,7 @@ fn main() -> rltk::BError {
   gs.ecs.register::<Renderable>();
   gs.ecs.register::<Player>();
 
-  gs.ecs.insert(new_map());
+  gs.ecs.insert(new_map_rooms_and_corridors());
 
   gs.ecs
     .create_entity()
