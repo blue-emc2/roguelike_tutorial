7c54c5a046ac7ccf6a5c05434e3f1a5530b7caca
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2020-08-14 09:18:13 +0900

    プレイヤーの初期位置を動的にした

--- src/map.rs
+++ src/map.rs
@@ -97,7 +97,7 @@ fn apply_vertical_tunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32) {
   }
 }
 
-pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
+pub fn new_map_rooms_and_corridors() -> (Vec<Rect>, Vec<TileType>) {
   let mut map = vec![TileType::Wall; 80 * 50];
 
   let mut rooms: Vec<Rect> = Vec::new();
@@ -138,5 +138,5 @@ pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
     }
   }
 
-  map
+  (rooms, map)
 }
