8d2d21ac937cb7967997e11931811a3b30b7656c
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2020-08-15 23:01:36 +0900

    ゴブリンとオークを表示

--- src/main.rs
+++ src/main.rs
@@ -79,14 +79,22 @@ fn main() -> rltk::BError {
     })
     .build();
 
+  let mut rng = rltk::RandomNumberGenerator::new();
   // skip(1): ゲーム開始時にプレイヤーと同じ部屋に敵を配置させない
   for room in map.rooms.iter().skip(1) {
     let (x, y) = room.center();
+    let glyph;
+    let roll = rng.roll_dice(1, 2);
+    match roll {
+      1 => glyph = rltk::to_cp437('g'),
+      _ => glyph = rltk::to_cp437('o'),
+    }
+
     gs.ecs
       .create_entity()
       .with(Position { x, y })
       .with(Renderable {
-        glyph: rltk::to_cp437('g'),
+        glyph: glyph,
         fg: RGB::named(rltk::RED),
         bg: RGB::named(rltk::BLACK),
       })
